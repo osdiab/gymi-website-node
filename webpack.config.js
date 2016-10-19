bd635baacc9038c886cdec0ced10e0ce35bd8935
Omar Diab <omar.diab@clever.com>
2016-10-19 12:05:46 -0400

    add babel polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 module.exports = {
   entry: [
     'webpack-hot-middleware/client',
+    'babel-polyfill',
     path.join(__dirname, 'frontend_app.js'),
   ],
   output: {
