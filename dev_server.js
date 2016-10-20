9feb6a7657d2b96a0b68231322e653981c1d5a0c
Omar Diab <omar.diab@clever.com>
2016-10-20 16:23:16 -0400

    split out the router for the backend

--- dev_server.js
+++ dev_server.js
@@ -8,19 +8,25 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from './webpack.config.js';
 
-import createApp from './backend_app';
+import router from './backend_app/router';
 
 const app = express();
 const port = 3000;
 const compiler = webpack(webpackConfig);
 
+// hook in webpack-compiled and bundled code (javascript, stylesheets...)
 app.use(webpackDevMiddleware(compiler, {
   publicPath: webpackConfig.output.publicPath,
   stats: { colors: true },
 }));
+
+// hot refresh changes to compiled code.
 app.use(webpackHotMiddleware(compiler, {
   log: console.log,
 }));
 
-const devApp = createApp(app);
-devApp.listen(port, () => console.log(`Listening on localhost:${port}`));
+// hook in the app's actual endpoints
+app.use(router());
+
+// listen for requests
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
