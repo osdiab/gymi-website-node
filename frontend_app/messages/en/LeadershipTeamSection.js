9b4e584aae8af8ba4e005764c768913bacf8c81d
Omar Diab <omar.diab@clever.com>
2016-10-29 19:11:18 -0700

    capitalize peoples names in translations page

--- frontend_app/messages/en/LeadershipTeamSection.js
+++ frontend_app/messages/en/LeadershipTeamSection.js
@@ -2,183 +2,183 @@
 
 export default [
   {
-    id: 'LeadershipTeamSection.christineWang.name',
+    id: 'LeadershipTeamSection.ChristineWang.name',
     defaultMessage: 'Christine Wang',
   },
   {
-    id: 'LeadershipTeamSection.christineWang.title',
+    id: 'LeadershipTeamSection.ChristineWang.title',
     defaultMessage: 'Co-founder and Co-president',
   },
   {
-    id: 'LeadershipTeamSection.christineWang.description',
+    id: 'LeadershipTeamSection.ChristineWang.description',
     defaultMessage: 'Hey everyone, I’m Christine and I’m currently a medical student at Harvard University. I recently graduated from Columbia where I studied Neuroscience and Hispanic Studies and co-founded GYMI with Shermin. GYMI has absolutely been a turning point in my life and has enabled me to meet many new friends, connect with new communities and work with passionate people around the world. I’m interested in applying the lessons I’ve learned with starting GYMI to new projects, maybe this time in healthcare! Outside of school and work, I love to try new foods with friends and explore!',
   },
   {
-    id: 'LeadershipTeamSection.sherminLuo.name',
+    id: 'LeadershipTeamSection.SherminLuo.name',
     defaultMessage: 'Shermin Luo',
   },
   {
-    id: 'LeadershipTeamSection.sherminLuo.title',
+    id: 'LeadershipTeamSection.SherminLuo.title',
     defaultMessage: 'Co-founder and Co-president',
   },
   {
-    id: 'LeadershipTeamSection.sherminLuo.description',
+    id: 'LeadershipTeamSection.SherminLuo.description',
     defaultMessage: 'Hi! I’m a senior at Columbia University studying Engineering Management System. My journey with GYMI has been the most rewarding experience in my life. I’m super excited to see how GYMI family will grow and thrive as a whole in the future! My personal goal is to build a fashion-tech company that can remove frictions in the way people shop and style themselves. I run away from cities and solo backpack to some place where no one knows who I am every couple months. Love reading and writing. Have a passion for street dance, music, food and puppies. And I’m an aurora chaser.',
   },
   {
-    id: 'LeadershipTeamSection.yunQu.name',
+    id: 'LeadershipTeamSection.YunQu.name',
     defaultMessage: 'Yun Qu',
   },
   {
-    id: 'LeadershipTeamSection.yunQu.title',
+    id: 'LeadershipTeamSection.YunQu.title',
     defaultMessage: 'Vice President of Administration',
   },
   {
-    id: 'LeadershipTeamSection.yunQu.description',
+    id: 'LeadershipTeamSection.YunQu.description',
     defaultMessage: 'Hi! I am from Changzhou, China and study Chemical Engineering at Columbia University. I am currently the Vice President of Administration for GYMI and really enjoy the experience of working for GYMI and helping children in underprivileged areas of China.',
   },
   {
-    id: 'LeadershipTeamSection.harryGao.name',
+    id: 'LeadershipTeamSection.HarryGao.name',
     defaultMessage: 'Harry Gao',
   },
   {
-    id: 'LeadershipTeamSection.harryGao.title',
+    id: 'LeadershipTeamSection.HarryGao.title',
     defaultMessage: 'Vice President of Public Relations',
   },
   {
-    id: 'LeadershipTeamSection.harryGao.description',
+    id: 'LeadershipTeamSection.HarryGao.description',
     defaultMessage: 'Hey! I was born in Hangzhou, China and now studying at Schulich School of Business in Canada. Having served as a team leader with GYMI myself, I’m excited to be part of the exec team as VP of PR. The most fascinating thing in working in GYMI is that the valuable attributes in human nature gets to be transferred into real life projects and life-changing impacts on numerous students in developing areas.',
   },
   {
-    id: 'LeadershipTeamSection.keyingQue.name',
+    id: 'LeadershipTeamSection.KeyingQue.name',
     defaultMessage: 'Keying (Claire) Que',
   },
   {
-    id: 'LeadershipTeamSection.keyingQue.title',
+    id: 'LeadershipTeamSection.KeyingQue.title',
     defaultMessage: 'Vice President of Human Resources',
   },
   {
-    id: 'LeadershipTeamSection.keyingQue.description',
+    id: 'LeadershipTeamSection.KeyingQue.description',
     defaultMessage: 'I graduated from University of Minnesota with majors in Psychology and Human Resources Development in December 2015. I am now working full-time as a HR professional as well as being the Vice President of HR for GYMI. Born and raised in China, I really appreciate the journey I have taken since I was 16 to experience the US education, which made who I am and inspired me to join GYMI. Working with GYMI allows me to give back as it supports students with advanced access to discover who they are and pursue their interest. I enjoy traveling, reading, trying out new food, and wine tasting. Working on getting certified for scuba diving.',
   },
   {
-    id: 'LeadershipTeamSection.ellenXu.name',
+    id: 'LeadershipTeamSection.EllenXu.name',
     defaultMessage: 'Ellen Xu',
   },
   {
-    id: 'LeadershipTeamSection.ellenXu.title',
+    id: 'LeadershipTeamSection.EllenXu.title',
     defaultMessage: 'Vice President of Operations',
   },
   {
-    id: 'LeadershipTeamSection.ellenXu.description',
+    id: 'LeadershipTeamSection.EllenXu.description',
     defaultMessage: 'I’m Ellen, a senior majoring in Statistics and minoring in Business at Simon Fraser University in Canada. Started small, GYMI is on its way transforming into a mature non-profit organization with structured operations. Although the main activities are currently only happening in China, we hope to expand to other countries soon and help underprivileged children all around the world.',
   },
   {
-    id: 'LeadershipTeamSection.ronaWo.name',
+    id: 'LeadershipTeamSection.RonaWo.name',
     defaultMessage: 'Rona (Yanrong) Wo',
   },
   {
-    id: 'LeadershipTeamSection.ronaWo.title',
+    id: 'LeadershipTeamSection.RonaWo.title',
     defaultMessage: 'Vice President of Technology',
   },
   {
-    id: 'LeadershipTeamSection.ronaWo.description',
+    id: 'LeadershipTeamSection.RonaWo.description',
     defaultMessage: 'Nice seeing you here! I am a senior at Columbia University majoring in computer science. I was born in Shanghai, China, but raised near Boston, MA. I’ve always been passionate about primary education! Before joining GYMI, I taught english in China, volunteered in Boston’s Museum of Science, participated in America Reads, and lead computer science lessons for high school girls. Aside from all my volunteering, I like coding (--in fact, I made this website!--) and lion dancing (a.k.a. dancing in a large cardboard box)!',
   },
   {
-    id: 'LeadershipTeamSection.andrewTang.name',
+    id: 'LeadershipTeamSection.AndrewTang.name',
     defaultMessage: 'Andrew Tang',
   },
   {
-    id: 'LeadershipTeamSection.andrewTang.title',
+    id: 'LeadershipTeamSection.AndrewTang.title',
     defaultMessage: 'Vice President of Finance',
   },
   {
-    id: 'LeadershipTeamSection.andrewTang.description',
+    id: 'LeadershipTeamSection.AndrewTang.description',
     defaultMessage: 'Hi! I am a student at Columbia University studying Operations Research. I have had a great amount of experience mentoring children and knows what to expect from mentoring. My hobbies, other then keeping track of money, include sports, lifting heavy objects and dropping heavy objects.',
   },
   {
-    id: 'LeadershipTeamSection.yilinWei.name',
+    id: 'LeadershipTeamSection.YilinWei.name',
     defaultMessage: 'Yilin Wei',
   },
   {
-    id: 'LeadershipTeamSection.yilinWei.title',
+    id: 'LeadershipTeamSection.YilinWei.title',
     defaultMessage: 'Co-president of Education',
   },
   {
-    id: 'LeadershipTeamSection.yilinWei.description',
+    id: 'LeadershipTeamSection.YilinWei.description',
     defaultMessage: 'I am a graduate student in the University of Minnesota—Twin Cities and my program is Educational Leadership and Policy. My undergraduate major was Dramatic Literature and Script Writing for films and TV shows. I worked as a middle school English teacher in an international school in China for four years. Before that, I also thought Mandarin to international adults and Chinese literature to Chinese middle schoolers. I am highly interested in intercultural communication, feminist critical theories, and gender studies. Those interests have provided me a fundamental possibility of being a successful graduate student in the education field. Overall, my wide range of interests have already confirmed my tendency of intercultural and international development of education. I hope through GYMI, I will be able to help Chinese students with more study skills and perspectives about life. I believe I also can improve my abilities in all aspects with the help of all the colleagues.',
   },
   {
-    id: 'LeadershipTeamSection.youzhihangDeng.name',
+    id: 'LeadershipTeamSection.YouzhihangDeng.name',
     defaultMessage: 'Youzhihang Deng',
   },
   {
-    id: 'LeadershipTeamSection.youzhihangDeng.title',
+    id: 'LeadershipTeamSection.YouzhihangDeng.title',
     defaultMessage: 'Marketing Director',
   },
   {
-    id: 'LeadershipTeamSection.youzhihangDeng.description',
+    id: 'LeadershipTeamSection.YouzhihangDeng.description',
     defaultMessage: 'Hi! I’m from Chengdu, China and now study at Columbia University. I’m a prospective Economics-Political Science major with strong interests in social justice issues. I decided to become part of the team as I view GYMI as an innovative and effective mentorship program, beneficial to both the underprivileged students and the mentors.',
   },
   {
-    id: 'LeadershipTeamSection.graceZheng.name',
+    id: 'LeadershipTeamSection.GraceZheng.name',
     defaultMessage: 'Grace (Zhonghui) Zheng',
   },
   {
-    id: 'LeadershipTeamSection.graceZheng.title',
+    id: 'LeadershipTeamSection.GraceZheng.title',
     defaultMessage: 'Recruitment Director',
   },
   {
-    id: 'LeadershipTeamSection.graceZheng.description',
+    id: 'LeadershipTeamSection.GraceZheng.description',
     defaultMessage: 'Grace is born in Shanghai, and now studies Financial Economics at Columbia University. She is dedicated to inequality and volunteering. During her two years of study in Hong Kong, she volunteered in various social projects such as Hong Kong Homeless Population Estimation project and Food Angel Kitchen. She is very enthusiastic about youth’s education, which is her motivation to join GYMI as event coordinating and property chair.',
   },
   {
-    id: 'LeadershipTeamSection.davidRyu.name',
+    id: 'LeadershipTeamSection.DavidRyu.name',
     defaultMessage: 'David Ryu',
   },
   {
-    id: 'LeadershipTeamSection.davidRyu.title',
+    id: 'LeadershipTeamSection.DavidRyu.title',
     defaultMessage: 'Branding Manager',
   },
   {
-    id: 'LeadershipTeamSection.davidRyu.description',
+    id: 'LeadershipTeamSection.DavidRyu.description',
     defaultMessage: 'Hey guys! I’m a designer and illustrator hailing from School of Visual Arts. Working for GYMI has been both gratifying and humbling experience, and I am very excited to see how the organization will expand. I have big hearts for anything narratives, such as books, movies, and even games. My dream is to work on my own story one day, and share it with the rest of the world. I love to read and draw, and always open for a great new music. I like to try foods that I’ve never tried before (although it usually brings back diminishing results). I have a habit of clicking my tongue three times when I’m thinking. I like to go on long walks and think about basically everything.',
   },
   {
-    id: 'LeadershipTeamSection.gavinJiao.name',
+    id: 'LeadershipTeamSection.GavinJiao.name',
     defaultMessage: 'Gavin (Huanwen) Jiao',
   },
   {
-    id: 'LeadershipTeamSection.gavinJiao.title',
+    id: 'LeadershipTeamSection.GavinJiao.title',
     defaultMessage: 'Designer',
   },
   {
-    id: 'LeadershipTeamSection.gavinJiao.description',
+    id: 'LeadershipTeamSection.GavinJiao.description',
     defaultMessage: 'I am Gavin. I am originally from Shenzhen, China, currently living in Paris. As a multi-disciplinary designer, I am passionate about creating meaningful objects and experiences that truly connect with people. I have worked for frog design and other design studios globally to help people innovate. I hold a bachelor degree in product design from Central Saint Martins, and I have also studied in China Central Academy of Art and Rhode Island School of Design.',
   },
   {
-    id: 'LeadershipTeamSection.yangLi.name',
+    id: 'LeadershipTeamSection.YangLi.name',
     defaultMessage: 'Yang Li',
   },
   {
-    id: 'LeadershipTeamSection.yangLi.title',
+    id: 'LeadershipTeamSection.YangLi.title',
     defaultMessage: 'Project Manager',
   },
   {
-    id: 'LeadershipTeamSection.yangLi.description',
+    id: 'LeadershipTeamSection.YangLi.description',
     defaultMessage: 'Hi, my name is Yang Li; I am a member of Columbia University, Class of 2020. I have dedicated two years to GYMI, and I was a camp coordinator the summer of 2015 during which I taught creative writing and programming with Arduinos. I really think GYMI has the potential to change the world starting by educating kids in poverty about creative thinking. I hope to continue my work with GYMI and help GYMI grow.',
   },
   {
-    id: 'LeadershipTeamSection.samanthaLee.name',
+    id: 'LeadershipTeamSection.SamanthaLee.name',
     defaultMessage: 'Samantha Lee',
   },
   {
-    id: 'LeadershipTeamSection.samanthaLee.title',
+    id: 'LeadershipTeamSection.SamanthaLee.title',
     defaultMessage: 'Marketing Director',
   },
   {
-    id: 'LeadershipTeamSection.samanthaLee.description',
+    id: 'LeadershipTeamSection.SamanthaLee.description',
     defaultMessage: 'I am a junior at Barnard College of Columbia University. I’m studying neuroscience and behavior with a minor in sociology on the pre-med track. I was born and raised in New York City and am passionate about civil rights and social justice. As a first-generation college student, I credit my own accomplishments to my opportunities in education. By combatting educational inequality around the world with GYMI, I’m able to give back to a cause I value greatly.',
   },
 ];
