b9ff77c7c48c5584552fb506d899d9422be78ee9
Omar Diab <osdiab@gmail.com>
2016-10-30 15:04:17 -0700

    add seasons

---
+++ frontend_app/messages/en/seasons.js
@@ -0,0 +1,35 @@
+// NOTE: unsure if there is a better way to do this
+export default [
+  {
+    id: 'seasons.summer',
+    defaultMessage: 'Summer',
+  },
+  {
+    id: 'seasons.winter',
+    defaultMessage: 'Winter',
+  },
+  {
+    id: 'seasons.fall',
+    defaultMessage: 'Fall',
+  },
+  {
+    id: 'seasons.spring',
+    defaultMessage: 'Spring',
+  },
+  {
+    id: 'seasons.seasonalDate.summer',
+    defaultMessage: 'Summer {year}',
+  },
+  {
+    id: 'seasons.seasonalDate.winter',
+    defaultMessage: 'Winter {year}',
+  },
+  {
+    id: 'seasons.seasonalDate.fall',
+    defaultMessage: 'Fall {year}',
+  },
+  {
+    id: 'seasons.seasonalDate.spring',
+    defaultMessage: 'Spring {year}',
+  },
+];
---
+++ frontend_app/messages/translations/zh/seasons.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    id: 'seasons.summer',
+    defaultMessage: '暑期',
+  },
+  {
+    id: 'seasons.winter',
+    defaultMessage: '冬季',
+  },
+  {
+    id: 'seasons.fall',
+    defaultMessage: '秋季',
+  },
+  {
+    id: 'seasons.spring',
+    defaultMessage: '春季',
+  },
+  {
+    id: 'seasons.seasonalDate.summer',
+    defaultMessage: '{year}暑期',
+  },
+  {
+    id: 'seasons.seasonalDate.winter',
+    defaultMessage: '{year}冬季',
+  },
+  {
+    id: 'seasons.seasonalDate.fall',
+    defaultMessage: '{year}秋季',
+  },
+  {
+    id: 'seasons.seasonalDate.spring',
+    defaultMessage: '{year}春季',
+  },
+];
+
