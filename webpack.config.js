1cc0637a2a2274d0b2691df744676493e29fc79c
Omar Diab <osdiab@gmail.com>
2016-10-30 13:10:20 -0700

    backend webpack config (wip)

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,92 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
 const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-  entry: [
-    'webpack-hot-middleware/client',
-    'babel-polyfill',
-    path.join(__dirname, 'frontend_app', 'index.js'),
-  ],
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
-  },
-  module: {
-    devtool: 'eval-source-map',
-    loaders: [
-      {
-        test: /\.(eot|woff|svg|ttf|png)$/,
-        loader: 'url',
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-      },
-      {
-        test: /\.less$/,
-        loader: 'style!css!postcss!less',
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-      },
+module.exports = [
+  {
+    name: 'frontend_app',
+    entry: [
+      'webpack-hot-middleware/client',
+      'babel-polyfill',
+      path.join(__dirname, 'frontend_app', 'index.js'),
     ],
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx'],
+    },
+    module: {
+      devtool: 'eval-source-map',
+      loaders: [
+        {
+          test: /\.(eot|woff|svg|ttf|png)$/,
+          loader: 'url',
+        },
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+        },
+        {
+          test: /\.less$/,
+          loader: 'style!css!postcss!less',
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel',
+        },
+      ],
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+    postcss: [autoprefixer({ browsers: ['> 5% in CN', '> 5% in US', 'last 2 versions', 'ie >= 9'] })],
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-  postcss: [autoprefixer({ browsers: ['> 5% in CN', '> 5% in US', 'last 2 versions', 'ie >= 9'] })],
-};
-
+  {
+    name: 'backend_app',
+    entry: [
+      'babel-polyfill',
+      path.join(__dirname, 'backend_app', 'index.js'),
+    ],
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'backend.js',
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx'],
+      alias: {
+        public: path.join(__dirname, 'assets'),
+      },
+    },
+    externals: [nodeExternals()],
+    module: {
+      devtool: 'eval-source-map',
+      loaders: [
+        {
+          test: /\.(eot|woff|svg|ttf|png)$/,
+          loader: 'url',
+        },
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+        },
+        {
+          test: /\.less$/,
+          loader: 'style!css!postcss!less',
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel',
+        },
+      ],
+    },
+    target: 'node',
+    postcss: [autoprefixer({ browsers: ['> 5% in CN', '> 5% in US', 'last 2 versions', 'ie >= 9'] })],
+  },
+];
