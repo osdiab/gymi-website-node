730d254dd9fbf5e21a5ac92c6a4cfbf3a10efcb1
Omar Diab <omar.diab@clever.com>
2016-11-03 14:48:53 -0700

    make questions non duplicatble

--- backend_app/db/submissionQuestions.js
+++ backend_app/db/submissionQuestions.js
@@ -13,7 +13,10 @@ const destroy = id => new Promise((resolve, reject) => {
 });
 
 const create = title => new Promise((resolve, reject) => {
-  const query = 'INSERT INTO submission_questions (title) VALUES ($<title>) RETURNING id';
+  const query = `
+    INSERT INTO submission_questions (title) VALUES ($<title>)
+    ON CONFLICT (title) DO UPDATE SET archived = false
+    RETURNING id`;
   db.one(query, { title }).then(resolve).catch(reject);
 });
 
--- backend_app/db/topics.js
+++ backend_app/db/topics.js
@@ -13,9 +13,9 @@ const destroy = id => new Promise((resolve, reject) => {
 
 const create = title => new Promise((resolve, reject) => {
   const query = `
-  INSERT INTO topics (title) VALUES ($<title>)
-  ON CONFLICT (title) DO UPDATE SET archived = false
-  RETURNING id`;
+    INSERT INTO topics (title) VALUES ($<title>)
+    ON CONFLICT (title) DO UPDATE SET archived = false
+    RETURNING id`;
   db.one(query, { title }).then(resolve).catch(reject);
 });
 
