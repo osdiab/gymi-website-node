1095fbd9be93ae9e2ce431707f98d47764566657
Omar Diab <omar.diab@clever.com>
2016-11-03 16:28:01 -0700

    add https handling to app

--- backend_app/index.js
+++ backend_app/index.js
@@ -2,10 +2,23 @@
 // Defines an express app that runs the boilerplate codebase.
 
 import express from 'express';
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
 
 import createRouter from './router';
 
-const port = 3000;
 const app = express();
 app.use(createRouter());
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  const sslOptions = {
+    key: fs.readFileSync(process.env.PRIVKEY_CERT_LOC),
+    cert: fs.readFileSync(process.env.FULLCHAIN_CERT_LOC),
+  };
+  http.createServer(app).listen(80);
+  https.createServer(sslOptions, app).listen(443);
+} else {
+  const port = 3000;
+  http.createServer(app).listen(3000, () => console.log(`Listening on port ${port}`));
+}
