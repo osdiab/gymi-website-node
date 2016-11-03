89d0c9c0d49ca1798e58708c27d76de0519a02e9
Omar Diab <omar.diab@clever.com>
2016-11-03 14:49:52 -0700

    add submission questions endpoints

--- backend_app/routes/submissionQuestions.js
+++ backend_app/routes/submissionQuestions.js
@@ -1,10 +1,32 @@
-const NOT_YET_IMPLEMENTED = (req, res) => {
-  res.status(501);
-  res.send('Not yet implemented!');
-};
+import _ from 'lodash';
+
+import { ApplicationError } from '../errors';
+import submissionQuestionsDb from '../db/submissionQuestions';
 
 export default {
-  create: NOT_YET_IMPLEMENTED,
-  destroy: NOT_YET_IMPLEMENTED,
-  list: NOT_YET_IMPLEMENTED,
+  create: (req, res, next) => {
+    const { title } = req.body;
+    if (_.isEmpty(title)) {
+      throw new ApplicationError('title is required', 400, { requiredFields: ['title'] });
+    }
+
+    submissionQuestionsDb.create(title).then((id) => {
+      res.send({ data: { id } });
+    }).catch(next);
+  },
+  destroy: (req, res, next) => {
+    const id = req.params.id || req.body.id;
+    if (_.isEmpty(id)) {
+      throw new ApplicationError('id is required', 400, { requiredFields: ['id'] });
+    }
+
+    submissionQuestionsDb.destroy(id).then(() => {
+      res.sendStatus(204);
+    }).catch(next);
+  },
+  list: (req, res, next) => {
+    submissionQuestionsDb.list().then(
+      questions => res.send(questions)
+    ).catch(next);
+  },
 };
