44325248db75b1896c20b5faa1f2ab0529335342
Omar Diab <omar.diab@clever.com>
2016-11-04 15:30:06 -0700

    add some styling to not found page

--- frontend_app/components/pages/NotFoundPage.jsx
+++ frontend_app/components/pages/NotFoundPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Button from '../Button';
+import './NotFoundPage.less';
 
 export default function NotFoundPage() {
   return (
-    <div>
+    <div className="NotFoundPage">
       <h1>Uh oh!</h1>
-      <h2>This page does not exist.</h2>
+      <h2>404: This page does not exist.</h2>
       <Button action={{ href: '/' }}>Return home</Button>
     </div>
   );
