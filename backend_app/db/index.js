3da30c3afe602e2f083afe53c0921aba98908ae9
Omar Diab <osdiab@gmail.com>
2016-11-05 16:28:14 -0700

    support separate dev and prod dbs

--- backend_app/db/index.js
+++ backend_app/db/index.js
@@ -1,12 +1,20 @@
 import pgPromise from 'pg-promise';
 
-const db = pgPromise()({
+const dbConfig = process.env.NODE_ENV === 'production' ? {
+  host: process.env.GYMI_WEBSITE_PROD_DB_HOST,
+  database: process.env.GYMI_WEBSITE_PROD_DB_DATABASE,
+  user: process.env.GYMI_WEBSITE_PROD_DB_USERNAME,
+  password: process.env.GYMI_WEBSITE_PROD_DB_PASSWORD,
+  port: 5432,
+  ssl: true,
+} : {
   host: process.env.GYMI_WEBSITE_DEV_DB_HOST,
   database: process.env.GYMI_WEBSITE_DEV_DB_DATABASE,
   user: process.env.GYMI_WEBSITE_DEV_DB_USERNAME,
   password: process.env.GYMI_WEBSITE_DEV_DB_PASSWORD,
   port: 5432,
   ssl: true,
-});
+};
+const db = pgPromise()(dbConfig);
 
 export default db;
