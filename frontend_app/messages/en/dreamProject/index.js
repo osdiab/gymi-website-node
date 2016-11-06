fe111fdefdea7ad8ed12f9d1e129b39ddf2db904
Omar Diab <osdiab@gmail.com>
2016-11-06 00:27:21 -0700

    add Profile messages

--- frontend_app/messages/en/dreamProject/index.js
+++ frontend_app/messages/en/dreamProject/index.js
@@ -1,9 +1,11 @@
 /* eslint-disable max-len */
 import navigation from './navigation';
 import layout from './layout';
+import profile from './profile';
 
 export default [
   ...navigation,
   ...layout,
+  ...profile,
 ];
 
---
+++ frontend_app/messages/en/dreamProject/profile.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 'dreamProject.profile.interests.title',
+    defaultMessage: 'Fields of Interests',
+  },
+  {
+    id: 'dreamProject.profile.interests.changeTags',
+    defaultMessage: 'Change Tags',
+  },
+  {
+    id: 'dreamProject.profile.interests.primary',
+    defaultMessage: 'Primary',
+  },
+  {
+    id: 'dreamProject.profile.interests.other',
+    defaultMessage: 'Other',
+  },
+  {
+    id: 'dreamProject.profile.submissions.title',
+    defaultMessage: 'Submissions',
+  },
+  {
+    id: 'dreamProject.profile.account.title',
+    defaultMessage: 'Account',
+  },
+  {
+    id: 'dreamProject.profile.account.settings',
+    defaultMessage: 'Settings',
+  },
+  {
+    id: 'dreamProject.profile.account.changePassword',
+    defaultMessage: 'Change password',
+  },
+];
