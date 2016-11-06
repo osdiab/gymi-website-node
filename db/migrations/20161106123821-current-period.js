132825dcdc3d21af85dc775a91b1b8d2705bb052
Omar Diab <osdiab@gmail.com>
2016-11-06 06:50:58 -0800

    add migration for current period table

---
+++ db/migrations/20161106123821-current-period.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+  db.createTable('currentPeriod', {
+    periodId: { type: 'int', notNull: true,
+      foreignKey: {
+        name: 'foreignKey_periodId_fk',
+        table: 'periods',
+        rules: {
+          onDelete: 'CASCADE',
+          onUpdate: 'RESTRICT'
+        },
+        mapping: 'id'
+      }
+    },
+  }, callback);
+};
+
+exports.down = function(db) {
+  db.dropTable('currentPeriod', callback);
+};
+
+exports._meta = {
+  "version": 1
+};
