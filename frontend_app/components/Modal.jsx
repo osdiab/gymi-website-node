fd00da601e2471b5ff831f85fe133087e898c8ce
Omar Diab <osdiab@gmail.com>
2016-11-08 00:21:28 -0800

    pure CSS modal

--- frontend_app/components/Modal.jsx
+++ frontend_app/components/Modal.jsx
@@ -8,20 +8,6 @@ const DEFAULT_WIDTH = 400;
 export default class Modal extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { windowHeight: window.innerHeight };
-    this.handleKeyUp = this.handleKeyUp.bind(this);
-    this.handleResize = this.handleResize.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('keyup', this.handleKeyUp);
-    window.addEventListener('resize', this.handleResize);
-    this.handleResize();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keyup', this.handleKeyUp);
-    window.removeEventListener('resize', this.handleResize);
   }
 
   handleKeyUp(e) {
@@ -30,17 +16,10 @@ export default class Modal extends React.Component {
     }
   }
 
-  handleResize() {
-    this.setState({ windowHeight: window.innerHeight });
-  }
-
   render() {
     const windowStyle = {
       width: `${this.props.width}px`,
-      marginLeft: `-${this.props.width / 2}px`,
     };
-    // The content is max 90% of the window height less 60px (height of the header)
-    const contentStyle = { maxHeight: (this.state.windowHeight * 0.9) - 60 };
     return (
       <div className="Modal">
         <div className="Modal--background" onClick={this.props.closeModal} />
@@ -49,7 +28,7 @@ export default class Modal extends React.Component {
             <button className="Modal--close" onClick={this.props.closeModal} />
             <h2>{this.props.title}</h2>
           </header>
-          <div style={contentStyle} className="Modal--window--content">
+          <div className="Modal--window--content">
             {this.props.children}
           </div>
         </div>
