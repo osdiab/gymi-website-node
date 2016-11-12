c574cad5132f7a6b88288881ff928a5f6ee4876b
Omar Diab <omar.diab@clever.com>
2016-11-11 18:38:07 -0800

    make footer fill height

--- frontend_app/components/SiteLayout.jsx
+++ frontend_app/components/SiteLayout.jsx
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import 'react-select/dist/react-select.css';
 
+import { hideModal as hideModalAction } from '../actions';
 import SiteNavigation from './SiteNavigation';
 import Footer from './Footer';
 import LogInModal from './LogInModal';
@@ -9,7 +10,7 @@ import SignUpModal from './SignUpModal';
 
 import './SiteLayout.less';
 
-export function SiteLayoutView({ children, modalId, modalProps, loggedIn }) {
+export function SiteLayoutView({ children, modalId, modalProps, loggedIn, hideModal }) {
   let modal;
   if (modalId) {
     switch (modalId) {
@@ -17,7 +18,10 @@ export function SiteLayoutView({ children, modalId, modalProps, loggedIn }) {
         modal = <LogInModal {...modalProps} />;
         break;
       case 'signup':
-        if (loggedIn) break;
+        if (loggedIn) {
+          hideModal();
+          break;
+        }
         modal = <SignUpModal {...modalProps} />;
         break;
       default:
@@ -43,6 +47,7 @@ SiteLayoutView.propTypes = {
   modalId: PropTypes.node,
   modalProps: PropTypes.object, // eslint-disable-line react/forbid-prop-types
   loggedIn: PropTypes.bool.isRequired,
+  hideModal: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
@@ -53,5 +58,11 @@ function mapStateToProps(state) {
   };
 }
 
-const SiteLayout = connect(mapStateToProps)(SiteLayoutView);
+function mapDispatchToProps(dispatch) {
+  return {
+    hideModal: () => dispatch(hideModalAction()),
+  };
+}
+
+const SiteLayout = connect(mapStateToProps, mapDispatchToProps)(SiteLayoutView);
 export default SiteLayout;
