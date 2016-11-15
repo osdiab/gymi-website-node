f2c0d619e1f12846096522eb9aecf1cc53f1f7fc
Omar Diab <osdiab@gmail.com>
2016-11-15 01:16:57 -0800

    add script for checking presence of all translations

---
+++ scripts/checkTranslations.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+import _ from 'lodash';
+
+import { formatMessages } from '../frontend_app/messages';
+import unformattedEn from '../frontend_app/messages/en';
+import unformattedTranslations from '../frontend_app/messages/translations';
+
+const definedMessages = _.keys(formatMessages(unformattedEn));
+for (const lang of _.keys(unformattedTranslations)) {
+  const definedTranslations = _.keys(formatMessages(unformattedTranslations[lang]));
+  const missing = _.difference(definedMessages, definedTranslations);
+  const extraneous = _.difference(definedTranslations, definedMessages);
+  console.log(`Language '${lang}'`);
+  console.log('----------------');
+  console.log('Extra translations (not in en):\n');
+  console.log(extraneous);
+  console.log('\nMissing translations:\n');
+  console.log(missing);
+}
