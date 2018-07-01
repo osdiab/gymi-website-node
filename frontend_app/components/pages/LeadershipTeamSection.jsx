e2fc2818d333f1cf37fbf18622b24f54b2cf9f67
Omar Diab <me@omardiab.com>
2018-07-01 14:29:44 -0700

    support no bio, new leadership

--- frontend_app/components/pages/LeadershipTeamSection.jsx
+++ frontend_app/components/pages/LeadershipTeamSection.jsx
@@ -8,24 +8,49 @@ require('./LeadershipTeamSection.less');
 const teamMessages = messages.LeadershipTeamSection;
 
 const TEAM_MEMBER_IDS = [
-  'ChristineWang', 'SherminLuo', 'YunQu', 'HarryGao', 'KeyingQue', 'EllenXu', 'RonaWo',
-  'AndrewTang', 'YilinWei', 'YouzhihangDeng', 'GraceZheng', 'DavidRyu', 'GavinJiao', 'YangLi',
-  'SamanthaLee',
+  'ChristineWang',
+  'SherminLuo',
+  'YunQu',
+  'YilinWei',
+  'ShantingChen',
+  'NingLu',
+  'KeyingQue',
+  'YuqingZhang',
+  'PoppyTian',
+  'KelinLi',
+  'QiaoqiaoDong',
 ];
 
 export default function LeadershipTeamSection() {
   return (
     <section className="LeadershipTeamSection">
-      { TEAM_MEMBER_IDS.map(id => (
-        <div className="LeadershipTeamSection--profile" key={id} tabIndex="0">
+      {TEAM_MEMBER_IDS.map(id => (
+        <div
+          className="LeadershipTeamSection--profile"
+          key={id}
+          // tabIndex="0" // TODO: uncomment when bios present
+        >
           <img role="presentation" src={`/media/team_photos/${id}.png`} />
-          <div className="LeadershipTeamSection--details">
-            <h4><FormattedMessage {...teamMessages[id].name} /></h4>
-            <h5><FormattedMessage {...teamMessages[id].title} /></h5>
-            <p><FormattedMessage {...teamMessages[id].description} /></p>
-          </div>
+          <h4>
+            <FormattedMessage {...teamMessages[id].name} />
+          </h4>
+          <h5>
+            <FormattedMessage {...teamMessages[id].title} />
+          </h5>
+          {/* TODO: uncomment when bios are present */}
+          {/* <div className="LeadershipTeamSection--details">
+            <h4>
+              <FormattedMessage {...teamMessages[id].name} />
+            </h4>
+            <h5>
+              <FormattedMessage {...teamMessages[id].title} />
+            </h5>
+            <p>
+              <FormattedMessage {...teamMessages[id].description} />
+            </p>
+          </div> */}
         </div>
-      )) }
+      ))}
     </section>
   );
 }
